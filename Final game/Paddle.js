1abd180f4ab4bf99d146b234692f4a9b2ac6f355
Luckii59 <98205344+Luckii59@users.noreply.github.com>
2022-01-23 07:53:02 +0530

    New js files added

---
+++ Final game/Paddle.js
@@ -0,0 +1,15 @@
+class Paddle {
+	constructor(x) {
+		this.x = x;
+		this.y = height / 2; // info for examiners, height is height of display window
+		this.height = 80;
+		this.width = 20;
+	}
+	
+	
+	display() {
+		fill(255);
+		rect(this.x, this.y, this.width, this.height);
+	}
+
+}
---
+++ Final game/Sketch.js
@@ -0,0 +1,11 @@
+let playerPaddle;
+
+function setup() {
+  createCanvas(624, 351);
+  playerPaddle = new Paddle(26);
+}
+
+function draw() {
+  background(0);
+  playerPaddle.display();
+}
