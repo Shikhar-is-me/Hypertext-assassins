86819133f0b6dea083d4409d67aa47f60c3d298e
Luckii59 <98205344+Luckii59@users.noreply.github.com>
2022-01-23 08:18:31 +0530

    Updated and added new files

--- Final game/aiPaddle.js
+++ Final game/aiPaddle.js
@@ -12,3 +12,31 @@ function draw() {
   playerPaddle.display();
   aiPaddle.display();
 }
+
+function draw() {
+    background(0);
+      
+    playerPaddle.display();
+    aiPaddle.display();
+      
+    playerPaddle.update(); 
+    aiPaddle.update();
+    processAI();
+    	
+  ball.update(); 
+  ball.display(); 
+	
+}
+
+function processAI() {
+  let middleOfPaddle = aiPaddle.y + aiPaddle.height / 2;
+	
+  if (middleOfPaddle > ball.y) {
+    aiPaddle.isUp = true;
+    aiPaddle.isDown = false;
+  } else {
+    aiPaddle.isDown = true;
+    aiPaddle.isUp = false;
+
+  }
+}
